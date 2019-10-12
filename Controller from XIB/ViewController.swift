6b9dca8b803b9d7a751406af0ea6d2aeb38498b5
Alexander Algashev <algashev@mail.ru>
2019-10-12 14:32:18 +0300

    Initial Commit

---
+++ Controller from XIB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Controller from XIB
+//
+//  Created by Александр Алгашев on 12/10/2019.
+//  Copyright © 2019 Александр Алгашев. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
